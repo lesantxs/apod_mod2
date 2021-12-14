a98e14ebc0c676768e987d74bdc0fc2ad52826c1
lesantxs <l.sancarmo@gmail.com>
2021-12-14 04:02:25 -0300

    first commit

---
+++ encaminhar.js
@@ -0,0 +1,15 @@
+function encaminharNasa(){
+    return window.location.href = "https://www.nasa.gov/"
+}
+
+function encaminharAPOD(){
+    return window.location.href = "https://apod.nasa.gov/apod/astropix.html"
+}
+
+function encaminharGithub(){
+    return window.location.href = "https://github.com/lesantxs"
+}
+
+function encaminharLinkedin(){
+    return window.location.href = "https://www.linkedin.com/in/let%C3%ADcia-santos-772358225/"
+}
---
+++ script.js
@@ -0,0 +1,5 @@
+const data = document.querySelector("#data")
+const imagem = document.querySelector("#imagem")
+const titulo = document.querySelector("#titulo")
+const autor = document.querySelector("#autor")
+const explicacao = document.querySelector("#explicacao")
