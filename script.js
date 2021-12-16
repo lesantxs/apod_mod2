f8b0a1ec3394c0998b5a672c7e0194d86ac8261f
lesantxs <l.sancarmo@gmail.com>
2021-12-16 02:39:55 -0300

    consumo de api e funções

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-const data = document.querySelector("#data")
-const imagem = document.querySelector("#imagem")
-const titulo = document.querySelector("#titulo")
-const autor = document.querySelector("#autor")
-const explicacao = document.querySelector("#explicacao")
+function consulta() {
+    let data = $(".data").val()
+    const chave = 'dNNwrKhvOInpkMrKEmgsoja5JwVjMgnlOdiYIyDB'
+
+    $.ajax({
+        url: `https://api.nasa.gov/planetary/apod?api_key=${chave}&date=${data}`,
+        success: function (resultado) {
+            mostraResultado(resultado)
+            $(".data").css({border: "2px solid black"})
+            $("#texto").html(" ")
+        },
+        error: function () {
+            $(".data").css({border: "2px solid red"})
+            $("#texto").html("Enter a date from June 16th, 1995 to the current date.").css({color: "red"})
+        }
+    })
+}
+
+
+function mostraResultado(consulta) {
+    const data = $("#data")
+    const midia = $("#midia")
+    const titulo = $("#titulo")
+    const autor = $("#autor")
+    const explicacao = $("#explicacao")
+
+
+    data.html(moment(consulta.date).format('MMMM Do YYYY'))
+    titulo.html(consulta.title)
+    autor.html(`<strong>Media credit:</strong> ${consulta.copyright}`)
+    explicacao.html(`<strong>Explanation:</strong> ${consulta.explanation}`)
+
+
+    if (consulta.media_type == 'image') {
+        midia.html(`<img id="midiaJS" src="${consulta.url}" width: "500"/>`)
+    } else {
+        midia.html(`<iframe width="500" height="300" id="midiaJS" src="${consulta.url}?autoplay=1&mute=1">
+        </iframe>`)
+    }
+}
